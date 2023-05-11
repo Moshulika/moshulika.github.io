b56f53b94e8d0f746a3e38c32bb33230fe800a7a
Moshu <edwardnanescu@gmail.com>
2023-05-11 22:38:54 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+function github()
+{
+    window.location.href = 'https://github.com/Moshulika';
+}
+
+function skills()
+{
+    window.location.href = 'skills.html';
+}
+
+function hideCookies()
+{
+
+    document.getElementById("cookie").style.display = "none";
+
+}
+
+function paypal()
+{
+    window.location.href = 'https://paypal.me/moshudev';
+}
